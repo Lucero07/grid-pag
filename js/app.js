0030f689f288f1532f21b97e02b81d6899968642
Lucero07 <ing.lucero.ramos@gmail.com>
2017-05-03 16:58:01 -0500

    pag sin modal

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
 /*
  * Archivo principal de funcionalidad de JS
  */
+ var espacioImagen = document.getElementById("modal");
+var imgMostrar = document.getElementById("imagenNueva");
+var imagenes = document.getElementsByClassName("imagenes");
+var span = document.getElementById("close");
+
+// span.addEventListener("click",cerrar);
